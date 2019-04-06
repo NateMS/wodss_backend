930219984ce728432a93fa009f6c3a3cdeb881ee
Manuel Stutz <manuel.stutz@students.fhnw.ch>
2019-04-06 23:10:38 +0200

    Change precondition of getContracts from 500 to 412. Remove 412 "Duplicate Key" at POST, because no field is marked as "unique"

--- controllers/contract.controller.js
+++ controllers/contract.controller.js
@@ -1,5 +1,4 @@
 import Contract from '../models/contract';
-import Employee from "../models/employee";
 
 /**
  * Get all contracts
@@ -11,18 +10,19 @@ export function getContracts(req, res) {
     // todo: if unauthenticated, return 401
     // todo: if token invalid, return 401
 
-    var fromDate = req.query.fromDate;
-    var toDate = req.query.toDate;
+    const fromDate = req.query.fromDate;
+    const toDate = req.query.toDate;
     if(new Date(fromDate) > new Date(toDate)) {
-        res.status(500).json([]);
-    } else {
-        Contract.find().inRange(fromDate, toDate).sort('-dateAdded').exec((err, contracts) => {
-            if (err) {
-                res.status(500).send(err);
-            }
-            res.json(contracts);
-        });
+        res.status(412).end();  //I'd prefer Precondition Failed, because it's something the user should fix.
+        return;                 //then abort the request.
     }
+
+    Contract.inRange(fromDate, toDate).sort('-dateAdded').exec((err, contracts) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.json(contracts);
+    });
 }
 
 export function addContract(req, res) {
@@ -33,18 +33,15 @@ export function addContract(req, res) {
 
         console.error("fehler!");
         res.status(412).end();
-        return
+        return;
     }
 
     const newContract = new Contract(req.body);
 
     newContract.save((err, saved) => {
         if (err) {
-            if (err.message.indexOf('duplicate key error') > 0) {
-                res.status(412).send(err); //todo: check if 409 is correct status code according to API definition
-            } else {
-                res.status(500).send(err);
-            }
+            res.status(500).send(err);
+            //todo: remove comment - it's not possible to get a duplicate key error, because no field is marked as "unique"
         } else {
             res.json(saved);
         }
