30940f0c43e0930b0aec72fc4eeec9419c8d06fd
Nadim Salloum <nsalloum@sensational.ch>
2019-04-16 14:53:12 +0200

    add correct route path for /token

--- routes.js
+++ routes.js
@@ -9,43 +9,43 @@ const passport = require('passport');
 
 const mapEmployees = require('./services/employee.mapper');
 
-const requireAuth = passport.authenticate('jwt', {session: false}, null);
+const requireAuth = passport.authenticate('jwt', { session: false }, null);
 
-module.exports = function (app) {
-  app.get('/', function(req, res){
-    res.send({hi: 'there'})
-  });
+module.exports = function(app) {
+    app.get('/', function(req, res) {
+        res.send({ hi: 'there' })
+    });
 
-  /**
-   * Employee-Endpoint
-   */
-  app.get('/api/employee', requireAuth, mapEmployees.map, EmployeeController.getEmployees);
-  app.get('/api/employee/:id', requireAuth, mapEmployees.map, EmployeeController.getEmployee);
-  app.post('/api/employee', requireAuth, mapEmployees.map, EmployeeController.addEmployee);
-  app.delete('/api/employee/:id', requireAuth, mapEmployees.map, EmployeeController.deleteEmployee);
-  app.put('/api/employee/:id', requireAuth, mapEmployees.map, EmployeeController.updateEmployee);
+    /**
+     * Employee-Endpoint
+     */
+    app.get('/api/employee', requireAuth, mapEmployees.map, EmployeeController.getEmployees);
+    app.get('/api/employee/:id', requireAuth, mapEmployees.map, EmployeeController.getEmployee);
+    app.post('/api/employee', requireAuth, mapEmployees.map, EmployeeController.addEmployee);
+    app.delete('/api/employee/:id', requireAuth, mapEmployees.map, EmployeeController.deleteEmployee);
+    app.put('/api/employee/:id', requireAuth, mapEmployees.map, EmployeeController.updateEmployee);
 
-  /**
-   * Project-Endpoint
-   */
-  app.get('/api/project', requireAuth, mapEmployees.map, ProjectController.getProjects);
-  app.get('/api/project/:id', requireAuth, mapEmployees.map, ProjectController.getProject);
-  app.post('/api/project', requireAuth, mapEmployees.map, ProjectController.addProject);
-  app.delete('/api/project/:id', requireAuth, mapEmployees.map, ProjectController.deleteProject);
+    /**
+     * Project-Endpoint
+     */
+    app.get('/api/project', requireAuth, mapEmployees.map, ProjectController.getProjects);
+    app.get('/api/project/:id', requireAuth, mapEmployees.map, ProjectController.getProject);
+    app.post('/api/project', requireAuth, mapEmployees.map, ProjectController.addProject);
+    app.delete('/api/project/:id', requireAuth, mapEmployees.map, ProjectController.deleteProject);
 
-  /**
-   * Contract-Endpoint
-   */
-  app.get('/api/contract', requireAuth, mapEmployees.map, ContractController.getContracts);
-  app.get('/api/contract/:id', requireAuth, mapEmployees.map, ContractController.getContract);
-  app.post('/api/contract', requireAuth, mapEmployees.map, ContractController.addContract);
-  app.delete('/api/contract/:id', requireAuth, mapEmployees.map, ContractController.deleteContract);
-  app.put('/api/contract/:id', requireAuth, mapEmployees.map, ContractController.updateContract);
+    /**
+     * Contract-Endpoint
+     */
+    app.get('/api/contract', requireAuth, mapEmployees.map, ContractController.getContracts);
+    app.get('/api/contract/:id', requireAuth, mapEmployees.map, ContractController.getContract);
+    app.post('/api/contract', requireAuth, mapEmployees.map, ContractController.addContract);
+    app.delete('/api/contract/:id', requireAuth, mapEmployees.map, ContractController.deleteContract);
+    app.put('/api/contract/:id', requireAuth, mapEmployees.map, ContractController.updateContract);
 
-  /**
-   * JWT-Token-Endpoint
-   */
-  app.post('/token', TokenController.createToken);
-  //PUT on /token requires Auth, because it's only accessible if a token exists that can be renewed
-  app.put('/token', requireAuth, TokenController.refreshToken);
+    /**
+     * JWT-Token-Endpoint
+     */
+    app.post('/api/token', TokenController.createToken);
+    //PUT on /token requires Auth, because it's only accessible if a token exists that can be renewed
+    app.put('/api/token', requireAuth, TokenController.refreshToken);
 };
